040a20de31ce8b930883ce7a33c75ed730987697
OleksiiYatsenko <oyat@softserveinc.com>
2022-06-16 18:59:43 +0300

    Add sign by public key for test and add public key to assembly info class

--- ADFS-Solution/ADFS-Plug-in/AssemblyInfo.cs
+++ ADFS-Solution/ADFS-Plug-in/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
 
 [assembly: Guid("c0a113d4-da11-42d1-bb07-4b87550c0a83")]
-[assembly: InternalsVisibleTo("ADFS-Plug-in.Tests")]
+[assembly: InternalsVisibleTo("ADFS-Plug-in.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b1d3f90b7c32788008512124ceeeb5cde1b00edf45f701311ca3d49f8635ff11e56e71d687343ea2d3c3bf82001c91a0ad58fd652c68b18250a6e8b3df174b626e1ed303f0908bc4ff981cd840cb7e786315eeab6b6682155ca2f9b80a1a3818cd23b303368d4d33b49a2b7d4e1fbcf0a700c20cf672ebb0193090e1fe6ff1c6")]
